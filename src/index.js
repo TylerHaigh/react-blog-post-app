fd8d211c3d70fda7721f6aff611d7d1bbfe688a7
Tyler Haigh <tylerhaigh1994@me.com>
2018-02-15 19:13:29 +1100

    Add dummy reducer  and react router library

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 
 import App from './components/app.component'
+import reducers from './reducers/index.reducer';
 
 ReactDOM.render(
-    <App />,
+    <Provider store={createStore(reducers)} >
+      <App />
+    </Provider>,
   document.querySelector(".container")
 );
+
+
+// npm install --save react-router-dom@4.0.0
---
+++ src/reducers/index.reducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    dummy: function() { return [] }
+});
+
+export default rootReducer;
