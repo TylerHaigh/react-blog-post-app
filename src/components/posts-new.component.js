87b8db61c20ec00d206cc6a2d7c7b94eff90803a
Tyler Haigh <tylerhaigh1994@me.com>
2018-02-17 10:03:12 +1100

    Add css to form validation and cancel button

--- src/components/posts-new.component.js
+++ src/components/posts-new.component.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import {Field, reduxForm} from 'redux-form';
-
+import {Link} from 'react-router-dom';
 
 const inputField = (field, label) => {
+  
+  const {meta: { touched, error }} = field;
+  const className = `form-group ${touched && error ? 'has-danger' : ''}`
+  
   return (
-    <div className="form-group">
+    <div className={className}>
       <label>{field.label}</label>
       <input className="form-control" type="text" {...field.input} />
-      {field.meta.error}
+      
+      <div className="text-help">
+        {touched ? error : ''}
+      </div>
     </div>
   )
 }
@@ -30,7 +37,9 @@ class PostsNew extends Component {
           <Field name="categories" label="Categories" component={inputField} />
           <Field name="content" label="Post Content" component={inputField} />
 
-          <button type="submit" className="btn btn_primary">Submit</button>
+          <button type="submit" className="btn btn-primary">Submit</button>
+
+          <Link to="/" className="btn btn-danger">Cancel</Link>
         </form>
     );
   }
