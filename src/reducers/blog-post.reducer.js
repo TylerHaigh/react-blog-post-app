b9f8411335eab3f9b16c27682343d4a7a02c1237
Tyler Haigh <tylerhaigh1994@me.com>
2018-02-16 19:34:14 +1100

    Make api call on component load

--- src/reducers/blog-post.reducer.js
+++ src/reducers/blog-post.reducer.js
@@ -2,7 +2,7 @@ import _ from 'lodash'
 
 import { FETCH_POSTS } from '../actions/index';
 
-export default function(state, action) {
+export default function(state = null, action) {
 
     switch(action.type) {
         case FETCH_POSTS : return  _.mapKeys(action.payload.data, 'id');
--- src/reducers/index.reducer.js
+++ src/reducers/index.reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import BlogPostsReducer from 'blog-post.reducer'
+import BlogPostsReducer from './blog-post.reducer'
 
 const rootReducer = combineReducers({
     posts: BlogPostsReducer
