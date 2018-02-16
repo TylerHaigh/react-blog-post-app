720692757399897d9e44f3d58a6fbb2e12c70f08
Tyler Haigh <tylerhaigh1994@me.com>
2018-02-17 10:38:57 +1100

    Map fetched post to component state prop

--- src/components/posts-show.component.js
+++ src/components/posts-show.component.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import {fetchPost} from '../actions/index.action';
+
 
 class PostsShow extends Component {
+    
+    componentDidMount() {
+        const {id} = this.props.match.params;
+        this.props.fetchPost(id);
+
+    }
+    
     render() {
         return (
             <div>Posts Show</div>
@@ -8,4 +19,8 @@ class PostsShow extends Component {
     }
 }
 
-export default PostsShow
+function mapStateToProps({ posts }, ownProps) {
+    return { post : posts[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
