023b0e96649b1164800408210f43e4233dd33df1
Tyler Haigh <tylerhaigh1994@me.com>
2018-02-16 21:13:14 +1100

    Add reduc form reducer

--- src/reducers/index.reducer.js
+++ src/reducers/index.reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import {reducer as formReducer} from 'redux-form';
 
 import BlogPostsReducer from './blog-post.reducer'
 
 const rootReducer = combineReducers({
-    posts: BlogPostsReducer
+    posts: BlogPostsReducer,
+    form: formReducer
 });
 
 export default rootReducer;
