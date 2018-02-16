df69256f358663b475dd53e8b3d2de2fc7e3d087
Tyler Haigh <tylerhaigh1994@me.com>
2018-02-16 21:42:33 +1100

    Add validation checks to form

--- src/components/posts-new.component.js
+++ src/components/posts-new.component.js
@@ -13,20 +13,36 @@ const inputField = (field, label) => {
 
 
 class PostsNew extends Component {
- 
-
   render() {
 
     return (
         <form>
           <Field name="title" label="Title for Post" component={inputField} />
-          <Field name="tags" label="Tags" component={inputField} />
+          <Field name="categories" label="Categories" component={inputField} />
           <Field name="context" label="Post Content" component={inputField} />
         </form>
     );
   }
 }
 
+
+
+
+function validate(values) {
+  const errors = {};
+
+  // validate inputs from values object => { name: value }
+  if (!values.title || values.title.length < 3) { errors.title = "Enter a title that is at least 3 characters"; }
+  if (!values.categories) { errors.categories = "Enter some categories"; }
+  if (!values.content) { errors.content = "Enter some content"; }
+
+  // if errors is empty, then ok.
+  // if erros has any properties, then redux form assumes for is invalid
+  return errors
+}
+
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm' // unique name of form
 })(PostsNew);
