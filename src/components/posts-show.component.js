2b6f4bc011c115cf6b4b63f7611837faec3f51c4
Tyler Haigh <tylerhaigh1994@me.com>
2018-02-17 10:44:40 +1100

    Render post object

--- src/components/posts-show.component.js
+++ src/components/posts-show.component.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom'
 
 import {fetchPost} from '../actions/index.action';
 
@@ -13,8 +14,18 @@ class PostsShow extends Component {
     }
     
     render() {
+
+        const {post} = this.props;
+
+        if (!post) return  <div>Fetching...</div>;
+
         return (
-            <div>Posts Show</div>
+            <div>
+                <Link to="/">Back to Index</Link>
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
         );
     }
 }
